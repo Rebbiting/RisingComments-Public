5c60b3e58bafa85d4d17e8a394284576d9e3a3e1
Rebbiting <66968339+Rebbiting@users.noreply.github.com>
2020-08-03 23:26:15 +0100

    Easier to use a different Rebbit account.

--- rc.py
+++ rc.py
@@ -3,13 +3,20 @@ import time
 import time
 from praw.models import MoreComments
 
+CLIENT_ID = "CLIENT_ID"
+CLIENT_SECRET = "CLIENT_SECRET"
+USER_AGENT = "Type random shit here."
+USERNAME = "USERNAME"
+PASSWORD = "PASSWORD"
+LIVE = "LIVE"
+
 while True:
     try:
-        reddit = praw.Reddit(client_id = '5Y0G8MCHSf0c4Q',
-                             client_secret = 'zNBGH6xoCU7dg5mBFyA_ZCPUT9g',
-                             user_agent = 'Hopefully there are no problems',
-                             username = 'CommentThatGiveKarma',
-                             password = 'CommentThatGiveKarma')
+        reddit = praw.Reddit(client_id = CLIENT_ID,
+                             client_secret = CLIENT_SECRET,
+                             user_agent = USER_AGENT,
+                             username = USERNAME,
+                             password = PASSWORD)
 
         def rcomment():
             for submission in reddit.subreddit('All').rising(limit=25):
@@ -23,31 +30,14 @@ while True:
                                 continue
                             if not comment.distinguished and not submission.saved:
                                 ckpkp = 100 * comment.score / submission.score
-                                #cmnt = reddit.subreddit('RisingPostComments').submit('Rising comment:', 'PK: ' + str(submission.score) + ' CK: ' + str(comment.score) + ' CKPKP: {:.3f}'.format(ckpkp) + '% C: ' + str(submission.num_comments) + ' \n \nRising comment: \n \n>' + comment.body + '\n \n ^(Comment permalink: https://old.reddit.com' + comment.permalink + ')')
-                                #cmnt.mod.distinguish()
-                                #if submission.over_18:
-                                #    cmnt.mod.nsfw()
-                                #if submission.spoiler:
-                                #    cmnt.mod.spoiler()
-                                #cmnt.mod.flair(text=submission.subreddit.display_name)
-                                #print("PENIS! " + submission.subreddit.display_name)
-                                #comment.save()
                                 submission.save()
                                 now = time.time()
-                                #print("1")
                                 post_age = (now - submission.created_utc)/60
-                                #print("2")
                                 comment_age = (now - comment.created_utc)/60
-                                #print("3")
                                 post_kpm = submission.score / post_age
-                                #print("4")
                                 comment_kpm = comment.score / comment_age
-                                #print("5")
-                                #reddit.submission(id='g49xp9').reply('PK: ' + str(submission.score) + 'PA: {:.3}'.format(post_age / 60) + 'm PKPS: {:.3}'.format(post_kpm * 60) + ' CK: ' + str(comment.score) + ' CA: {:.3}'.format(comment_age / 60) + 'CKPM: {:.3}'.format(comment_kpm * 60) + ' CKPKP: {:.3f}'.format(ckpkp) + '% C: ' + str(submission.num_comments) + ' \n \nRising comment: \n \n>' + comment.body + '\n \n ^(Comment permalink: https://old.reddit.com' + comment.permalink + ')')
-                                #reddit.submission(id='iOct2z').reply('PK: ' + str(submission.score) + ' CK: ' + str(comment.score) + ' CKPKP: {:.3f}'.format(ckpkp) + '% C: ' + str(submission.num_comments) + ' \n \nRising comment: \n \n>' + comment.body + '\n \n ^(Comment permalink: https://old.reddit.com' + comment.permalink + ')')
-                                #reddit.live('15dw9z47l1x9p').contrib.add('PL: ' + str(submission.score) + ' CK: ' + str(comment.score) + ' CKPKP: {:.3f}'.format(ckpkp) + '% C: ' + str(submission.num_comments) + ' \n \nRising comment: \n \n>' + comment.body + '\n \n ^(Comment permalink: https://old.reddit.com' + comment.permalink + ')')
-                                reddit.live('15dw9z47l1x9p').contrib.add("PK: {0} | PT: {1}m |  PKPT: {2}/m | CK: {3} | CT: {4}m | CKPT: {5}/m | CKPPK: {6:.3f}% | C: {7}\n\nRising comment:\n\n> {8:.3500}\n\n^(Comment permalink: https://old.reddit.com{9})".format(submission.score, round(post_age,2) if round(post_age) > 0.01 else "<0.01", round(post_kpm,2) if round(post_kpm,2) > 0.01 else "<0.01", comment.score, round(comment_age,2) if round(comment_age,2) > 0.01 else "<0.01", round(comment_kpm,2) if round(comment_kpm,2) > 0.01 else "<0.01", ckpkp, submission.num_comments, comment.body, comment.permalink))
-                                print("PENIS 2!")
+                                contrib = reddit.live(LIVE).contrib.add("PK: {0} | PT: {1}m |  PKPT: {2}/m | CK: {3} | CT: {4}m | CKPT: {5}/m | CKPPK: {6:.3f}% | C: {7}\n\nRising comment:\n\n> {8:.3500}\n\n^(Comment permalink: https://old.reddit.com{9})".format(submission.score, round(post_age,2) if round(post_age) > 0.01 else "<0.01", round(post_kpm,2) if round(post_kpm,2) > 0.01 else "<0.01", comment.score, round(comment_age,2) if round(comment_age,2) > 0.01 else "<0.01", round(comment_kpm,2) if round(comment_kpm,2) > 0.01 else "<0.01", ckpkp, submission.num_comments, comment.body, comment.permalink))
+                                print(contrib)
                                 time.sleep(1)
                                 break
                         except Exception as e:
